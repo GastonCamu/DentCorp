cd4bee11a81cf3605dee1629bb808cfb3294c62d
Karen Cordoba <karen_cochi_02@hotmail.com>
2023-11-09 01:11:45 -0300

    crud de turnos y templates no terminados

--- DentCorpApp/urls.py
+++ DentCorpApp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from DentCorpApp import views
 
 urlpatterns = [
-    path('', views.home, name= "home")
+    path('', views.home, name= "home"),
+    path('turnos/', views.TurnosListView.as_view(), name="turnos_list"),
+    path('turnos/<uuid:pk>/', views.TurnosDetailView.as_view(), name="turnos_detail"),
+    path('turnos/', views.TurnosCreateView.as_view(), name="turnos_create"),
+    path('turnos/', views.TurnosUpdateView.as_view(), name="turnos_update"),
+    path('turnos/', views.TurnosDeleteView.as_view(), name="turnos_delete"),
 ]
