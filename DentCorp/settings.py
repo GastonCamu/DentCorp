7b93e45877873f01fd3ce4851075327ef19677c1
2329Lamas-Erick <lamaserick28@gmail.com>
2023-11-11 18:12:38 -0300

    fix merge

--- DentCorp/settings.py
+++ DentCorp/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'DentCorp/static'), )
+STATICFILES_DIRS = ()
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
