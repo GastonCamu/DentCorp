afb37947c5d9c7c892d24c7918437cd41f022707
Karen Cordoba <karen_cochi_02@hotmail.com>
2023-11-15 01:50:57 -0300

    personalizacion de las tablas users, rolxusuario y roles

--- DentCorpApp/models.py
+++ DentCorpApp/models.py
@@ -1,40 +1,43 @@
 from django.db import models
-from django.db import models
-from django.db import models
-from django.contrib.auth.models import AbstractGroup, AbstractUser, Group, Permission
+from django.contrib.auth.models import  AbstractUser, Group, Permission
 from DentCorp import settings
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
-import uuid
-
-from django.contrib.auth.models import AbstractUser, Group, Permission
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-from .models import User
 
 
-class Group(AbstractGroup):
+class Roles(Group):
     nombre_rol = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre_rol
     
-class CustomUserGroup(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    fecha_alta_usu_grupo = models.DateField()
-    fecha_baja_usu_grupo = models.DateField()
+    
+class Provincias(models.Model):
+    nom_prov = models.CharField(max_length=50)
 
     def __str__(self):
-        return f'{self.user.email} - {self.group.nombre_rol}'    
+        return self.nom_prov    
+    
+class Ciudades(models.Model):
+    nom_ciu = models.CharField(max_length=20)
+    id_prov = models.ForeignKey(Provincias, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nom_ciu    
     
 class User(AbstractUser):
-    roles = models.ManyToManyField(Group, related_name='DentCorpApp_users_groups')
+    roles = models.ManyToManyField(Roles, through='RolXUsuario' ,related_name='DentCorpApp_users_roles')
     user_permissions = models.ManyToManyField(Permission, related_name='DentCorpApp_users_permissions')
     image = models.ImageField(upload_to='users/%Y/%m/%d', null=True, blank=True)
     email = models.EmailField(_('email address'), unique=True)
     fecha_alta_usu = models.DateField()
     fecha_baja_usu = models.DateField()
+    dni_usu = models.CharField(max_length=9)
+    nom_usu = models.CharField(max_length=50)
+    ape_usu = models.CharField(max_length=50)
+    dom_usu = models.CharField(max_length=50)
+    tel_usu = models.CharField(max_length=14)
+    id_ciu = models.ForeignKey(Ciudades, on_delete=models.CASCADE)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -45,13 +48,25 @@ class User(AbstractUser):
         
         return '{}{}'.format(settings.STATIC_URL, 'img/user-default.png')
     
+    def get_absolute_url(self):
+        return reverse('infoUsuarios', args=[str(self.id)])
+
     def __str__(self):
         return f'{self.dni_usu}, {self.dom_usu}, {self.tel_usu}, {self.email}'
+        
+class RolXUsuario(models.Model):
+    id_usu = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_rol = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    fecha_alta_usu = models.DateField()
+    fecha_baja_usu = models.DateField()
 
+    def get_absolte_url(self):
+        return reverse ('infoRolXUsuario', args=[str(self.id)])
 
+    def __str__(self):
+        return f'{self.fecha_alta_usu}, {self.fecha_baja_usu}'    
 
     
-
 class Especialidades(models.Model):
     nombre_espec = models.CharField(max_length=50)
 
@@ -74,11 +89,7 @@ class ServiciosOdontologicos(models.Model):
     def __str__(self):
         return f'{self.nombre_serv_odon}, {self.costo_serv_odon}'
 
-class Provincias(models.Model):
-    nom_prov = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.nom_prov
 
 class Planes(models.Model):
     nombre_plan = models.CharField(max_length=20)
@@ -101,43 +112,7 @@ class PagosServExt(models.Model):
 
     def __str__(self):
         return f'{self.nombre_serv}, {self.fecha_cad_cont}'
-    
 
-
-class Ciudades(models.Model):
-    nom_ciu = models.CharField(max_length=20)
-    id_prov = models.ForeignKey(Provincias, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.nom_ciu
-
-class Usuarios(models.Model):
-    dni_usu = models.CharField(max_length=9)
-    nom_usu = models.CharField(max_length=50)
-    ape_usu = models.CharField(max_length=50)
-    dom_usu = models.CharField(max_length=50)
-    tel_usu = models.CharField(max_length=14)
-    email_usu = models.EmailField()
-    contra_usu = models.CharField(max_length=50)
-    id_ciu = models.ForeignKey(Ciudades, on_delete=models.CASCADE)
-
-    def get_absolute_url(self):
-        return reverse('infoUsuarios', args=[str(self.id)])
-
-    def __str__(self):
-        return f'{self.dni_usu}, {self.nom_usu}, {self.ape_usu}, {self.dom_usu}, {self.tel_usu}, {self.email_usu}, {self.contra_usu}'
-    
-class RolXUsuario(models.Model):
-    fecha_alta_usu = models.DateField()
-    fecha_baja_usu = models.DateField()
-    id_usu = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
-    id_rol = models.ForeignKey(Roles, on_delete=models.CASCADE)
-
-    def get_absolte_url(self):
-        return reverse ('infoRolXUsuario', args=[str(self.id)])
-
-    def __str__(self):
-        return f'{self.fecha_alta_usu}, {self.fecha_baja_usu}'
     
 class EspecXUsuario(models.Model):
     matricula = models.IntegerField()
@@ -171,8 +146,11 @@ class Cajas(models.Model):
     comentarios = models.CharField(max_length=100)
     id_rol_usu = models.ForeignKey(RolXUsuario, on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('infoCajas', args=[str(self.id)])
+
     def __str__(self):
-        return self.nom_prov
+        return f'{self.fecha_hr_ap_cj}, {self.fecha_hr_cr_cj}, {self.monto_ap_cj}, {self.monto_cr_cj}, {self.comentarios}'
 
 class FacturasServExt(models.Model):
     link_fact = models.CharField(max_length=200)
@@ -183,23 +161,30 @@ class FacturasServExt(models.Model):
     id_caja = models.ForeignKey(Cajas, on_delete=models.CASCADE)
     id_serv_ext = models.ForeignKey(PagosServExt, on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('infoFacturasServExt', args=[str(self.id)])
+
     def __str__(self):
-        return self.nom_prov
+        return f'{self.link_fact}, {self.costo_fact}, {self.fecha_cad_fact}, {self.fecha_pago_fact}, {self.comprobante_pago}'
+
 
 class PlanXCobertura(models.Model):
     porcentaje_cob = models.Field(max_length=3)
     id_plan = models.ForeignKey(Planes, on_delete=models.CASCADE)
     id_cob = models.ForeignKey(Coberturas, on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse ('infoPanXCobertura', args=[str(self.id)])
+
     def __str__(self):
-        return self.nom_prov
+        return self.porcentaje_cob
 
 class CoberturasXUsuario(models.Model):
     id_rol_usu = models.ForeignKey(RolXUsuario, on_delete=models.CASCADE)
     id_plan_cob = models.ForeignKey(PlanXCobertura, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.nom_prov
+    def get_absolute_url(self):
+        return reverse('infoCoberturasXUsuario', args=[str(self.id)])
 
 class Turnos(models.Model):
     fecha_hr_turno = models.DateTimeField()
@@ -209,8 +194,11 @@ class Turnos(models.Model):
     id_rol_usu = models.ForeignKey(RolXUsuario, on_delete=models.CASCADE)
     id_asig_cons = models.ForeignKey(AsignacionesConsultorio, on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('infoTurnos', args=[str(self.id)])
+
     def __str__(self):
-        return self.nom_prov
+        return f'{self.fecha_hr_turno}, {self.autorizado}'
 
 class FacturasOdontologicas(models.Model):
     costo_fact_pac = models.FloatField()
