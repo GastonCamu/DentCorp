c277defc16ba560c68f7fb563176209a350c1a44
Karen Cordoba <karen_cochi_02@hotmail.com>
2023-11-22 19:49:43 -0300

    control de urls

--- DentCorp/urls.py
+++ DentCorp/urls.py
@@ -17,9 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from DentCorpApp.views import register
 from django.views.generic import RedirectView
-from DentCorpApp.views import register
 from django.views.generic import RedirectView
 
 
