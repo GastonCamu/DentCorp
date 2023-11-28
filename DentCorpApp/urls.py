03847e32e660bc3eed20198d387ba072741797ce
Gastón Camú <camugaston2002@gmail.com>
2023-11-28 09:11:31 -0300

    fix filtrar

--- DentCorpApp/urls.py
+++ DentCorpApp/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('servicios-odontologicos/', ServiciosOdontologicosListView.as_view(), name = "servicios odontologicos"),
     
     path('consultorios/', ConsultoriosListView.as_view(), name='consultorios'),
-    path('pacientes/', PacientesListView.as_view(), name = "pacientes"),
-    path('pacientes/crear', PacientesCreateView.as_view(), name = "pacientes_create"),
     path('turnos/', TurnosListView.as_view(), name="turnos"),
     path('turnos/reservar/', TurnosCreateView.as_view(), name="turno_create"),
     path('turno/modificar/<pk>/', TurnosUpdateView.as_view(), name="turno_update"),
